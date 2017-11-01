e79d40ed36e715c546efef412efe2d6074824abc
nobody <nooneknows@whocares.aah>
2017-11-01 20:47:01 +0000

    Start testing of knotdns+unbound for dnssec

---
+++ testinfra/knotdns.py
@@ -0,0 +1,17 @@
+def test_knot_is_installed(host):
+    knot = host.package("knot2")
+    assert knot.is_installed
+    assert knot.version.startswith("2.5.3")
+
+       sysrc knot_enable=YES                                                                                                   
+           sysrc knot_config=/usr/local/etc/knot/knot.conf                                            
+               service knot start
+               pkg install -y unbound
+
+/var/db/knot/home.lan.zone exists
+
+/usr/local/etc/unbound/unbound.conf
+forward-addr pointing at knot server
+listening on 5353
+
+drill jail.home.lan @192.168.1.23
