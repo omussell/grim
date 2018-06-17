a60caf4ed0a297d15ac5a39337257b553becebed
Oliver Mussell <oliver.mussell@hotmail.co.uk>
2018-06-17 11:06:07 +0100

    Update local docs script and update tasks

--- local_docs.sh
+++ local_docs.sh
@@ -1,27 +1,27 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 set -ex
 #for type in stylesheets, images, fonts, javascripts, overview, design, implementation; do 
 for file in $(grep -Rl "/grim/stylesheets" ./docs); do
-	sed -i "s/\/grim\/stylesheets/\/stylesheets/" $file
+	sed -i '' "s/\/grim\/stylesheets/\/stylesheets/" $file
 done
 for file in $(grep -Rl "/grim/images" ./docs); do
-	sed -i "s/\/grim\/images/\/images/" $file
+	sed -i '' "s/\/grim\/images/\/images/" $file
 done
 for file in $(grep -Rl "/grim/fonts" ./docs); do
-	sed -i "s/\/grim\/fonts/\/fonts/" $file
+	sed -i '' "s/\/grim\/fonts/\/fonts/" $file
 done
 for file in $(grep -Rl "/grim/javascripts" ./docs); do
-	sed -i "s/\/grim\/javascripts/\/javascripts/" $file
+	sed -i '' "s/\/grim\/javascripts/\/javascripts/" $file
 done
 for file in $(grep -Rl "/grim/overview" ./docs); do
-	sed -i "s/\/grim\/overview/\/overview/" $file
+	sed -i '' "s/\/grim\/overview/\/overview/" $file
 done
 for file in $(grep -Rl "/grim/design" ./docs); do
-	sed -i "s/\/grim\/design/\/design/" $file
+	sed -i '' "s/\/grim\/design/\/design/" $file
 done
 for file in $(grep -Rl "/grim/implementation" ./docs); do
-	sed -i "s/\/grim\/implementation/\/implementation/" $file
+	sed -i '' "s/\/grim\/implementation/\/implementation/" $file
 done
 
-sudo /usr/local/bin/quark -d ~/grim/docs -p 80 &
+sudo quark -h localhost -p 80 -d ~/grim/docs &
 surf localhost:80 &
